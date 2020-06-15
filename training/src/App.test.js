33c48acebd6c02dd815a87728c3f99e199032091
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-06-15 20:02:29 +0200

    feat: pw5

--- training/src/App.test.js
+++ training/src/App.test.js
@@ -17,5 +17,19 @@ describe("<App />", () => {
     it("renders a substitle", () => {
       expect(container.querySelector("p")).toHaveTextContent("Bulma");
     });
+
+    it("should render a table", () => {
+      expect(container.querySelector("table")).toBeInTheDocument();
+    });
+
+    it("should render a 4 trs inside a tbody", () => {
+      expect(container.querySelectorAll("tbody tr").length).toBe(4);
+    });
+
+    it("should render the good second line", () => {
+      expect(
+        container.querySelector("tbody tr:nth-child(2) td:nth-child(1)")
+      ).toHaveTextContent("C-3PO");
+    });
   });
 });
