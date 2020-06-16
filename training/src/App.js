e47e6a82261e0259c272ef3e086d2a546c74f676
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-06-16 20:40:01 +0200

    feat: pw8

--- training/src/App.js
+++ training/src/App.js
@@ -1,57 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import PeopleList from "./PeopleList";
 import PeopleFilter from "./PeopleFilter";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: "",
-      people: [],
-      loading: true,
-    };
-  }
-
-  getInput = (newValue) => {
-    this.setState({ input: newValue });
-  };
-
-  componentDidMount = () => {
+function useStarWars() {
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
     fetch("https://swapi.dev/api/people/")
       .then((response) => response.json())
-      .then((body) =>
-        this.setState({
-          people: body.results,
-        })
-      )
-      .finally(() => this.setState({ loading: false }));
-  };
+      .then((body) => setPeople(body.results))
+      .finally(() => setLoading(false));
+  }, []);
 
-  render() {
-    const filteredPeople = this.state.people.filter((person) => {
-      return person.name.includes(this.state.input);
-    });
+  return [people, loading];
+}
 
-    if (this.state.loading) {
-      return (
-        <progress class="progress is-small is-primary" max="100"></progress>
-      );
-    }
+const App = () => {
+  const [input, setInput] = useState("");
+  const [people, loading] = useStarWars();
 
+  const filteredPeople = people.filter((person) => {
+    return person.name.includes(input);
+  });
+
+  if (loading) {
     return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Hello World</h1>
-          <p className="subtitle">
-            My first website with <strong>Bulma</strong>!x
-          </p>
-        </div>
-        <PeopleFilter onChangeHandler={this.getInput} />
-        <PeopleList people={filteredPeople} />
-      </section>
+      <progress className="progress is-small is-primary" max="100"></progress>
     );
   }
-}
+
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Hello World</h1>
+        <p className="subtitle">
+          My first website with <strong>Bulma</strong>!x
+        </p>
+      </div>
+      <PeopleFilter onChangeHandler={setInput} />
+      <PeopleList people={filteredPeople} />
+    </section>
+  );
+};
 
 export default App;
